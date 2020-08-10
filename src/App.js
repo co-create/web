e76dcdabd98edc8e47b1e230a1ca91e22db0abb4
Benjamin Kim <benkim96@gmail.com>
2020-08-10 14:24:51 -0700

    web changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-    Techgronomist! Unite jarrod 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  state = {
+    label: "",
+    data: [],
+  };
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://dev.techgronomist.com/api/1/iotClient/'); // fetching the data from api, before the page loaded
+      const ret = await res.json();
+      console.log(ret);
+      this.setState({
+        label:ret['label'],
+        data:ret['data'],
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.label}</h1>
+        <h1>{this.state.data}</h1>
+      <div style={{
+					backgroundColor: "white",
+					borderColor: "red",
+					borderWidth: 2,
+					height: 400,
+					margin: 50,
+					padding: 20,
+					flex: 1,
+					flexDirection: 'column',
+					justifyContent: 'flex-end',
+				}}>
+					<div style={{
+						backgroundColor: "blue",
+						height: 400 * this.state.data[0] / 301,
+					}}/>
+				</div>
+      </div>
+    );
+  }
 }
-
 export default App;
